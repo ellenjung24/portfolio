2cf67a4aa3b56354cec474fcda992b353bfb6278
DabeenJung <dabeenj24@gmail.com>
2022-01-08 22:53:38 -0800

    created skills.html and projects.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 document.getElementById("profile_button").addEventListener("click", showProf, false);
 document.getElementById("skills_button").addEventListener("click", showSki, false);
-document.getElementById("education_button").addEventListener("click", showEdu, false);
 document.getElementById("projects_button").addEventListener("click", showProj, false);
 document.getElementById("interests_button").addEventListener("click", showInt, false);
 
 function changeToPink() {
     document.getElementById("profile_button").style.backgroundColor = "#F7C5CC";
     document.getElementById("skills_button").style.backgroundColor = "#F7C5CC";
-    document.getElementById("education_button").style.backgroundColor = "#F7C5CC";
     document.getElementById("projects_button").style.backgroundColor = "#F7C5CC";
     document.getElementById("interests_button").style.backgroundColor = "#F7C5CC";
 }
@@ -22,12 +20,6 @@ function showSki() {
     changeToPink();
     document.getElementById("skills_button").style.backgroundColor = "#FCF6F5FF";
     document.getElementById("frame").src = "skills.html";
-}
-
-function showEdu() {
-    changeToPink();
-    document.getElementById("education_button").style.backgroundColor = "#FCF6F5FF";
-    document.getElementById("frame").src = "education.html";
 }
 
 function showProj() {
